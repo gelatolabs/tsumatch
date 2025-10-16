63f93ac43d4424561e11b76760f0dc53420649c1
Kyle Farwell <m@kfarwell.org>
2025-10-16 03:08:32 -0400

    fix?: builds

--- icons/generate.sh
+++ icons/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Generate Windows ICO, Apple Icon Image, and Android icons from icon.png
+# Generate Windows ICO, Apple Icon Image, iOS and Android icons from icon.png
 
 set -e
 cd "$(dirname "$0")"
@@ -18,6 +18,12 @@ for size in 32 64 256 512; do
 done
 iconutil -c icns -o icon.icns /tmp/tsumatch.iconset
 rm -rf /tmp/tsumatch.iconset
+
+# iOS
+mkdir -p ios.iconset
+for size in 20 29 40 58 60 76 80 87 120 152 167 180 1024; do
+    magick icon.png -resize ${size}x${size} ios.iconset/icon_${size}x${size}.png
+done
 
 # Android
 mkdir -p android/mipmap-mdpi android/mipmap-hdpi android/mipmap-xhdpi android/mipmap-xxhdpi android/mipmap-xxxhdpi
